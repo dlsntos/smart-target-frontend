1da98a75c581587ac97333d1f8d7aba7b476826c
Dean <deanlt.santos@gmail.com>
2025-07-27 12:26:51 +0800

    Add basic structure

---
+++ src/components/AdSection.jsx
@@ -0,0 +1,11 @@
+function AsSection() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="grid grid-cols-2 grid-rows-3 gap-4">
+        {}
+      </div>
+    </div>
+  );
+}
+
+export default AsSection;
