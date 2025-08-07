b5f225d10e3082aab9183a2981ab52d3be04b810
Dean <dtsantos1@student.hau.edu.ph>
2025-08-07 14:50:36 +0800

    debugged marquee animation by moving the config from the duplicate file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'scroll-horizontal': {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+        'scroll-vertical': {
+          '0%': { transform: 'translateY(0%)' },
+          '100%': { transform: 'translateY(-100%)' },
+        },
+      },
+      animation: {
+        'scroll-horizontal': 'scroll-horizontal 20s linear infinite',
+        'scroll-vertical': 'scroll-vertical 1s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
