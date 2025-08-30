4e5fd2003fe177c3dc0fb5b3443e58b6607144d1
Kurt Sardes <18kurt.sardes@gmail.com>
2025-08-31 02:29:46 +0800

    Synced with Ads.jsx

--- src/components/AdSection.jsx
+++ src/components/AdSection.jsx
@@ -1,21 +1,14 @@
-import { useEffect, useState } from "react";
 import Ads from "./Ads";
+
 function AdSection() {
-  
   return (
-  <div>
     <div className="flex items-center justify-center">
       <div className="grid grid-cols-2 grid-rows-3 gap-4">
-        <Ads />
-        <Ads />
-        <Ads />
-        <Ads />
-        <Ads />
-        <Ads />
+        {[...Array(6)].map((_, i) => (
+          <Ads key={i} index={i} total={6} />
+        ))}
       </div>
     </div>
-  </div>
-    
   );
 }
 
